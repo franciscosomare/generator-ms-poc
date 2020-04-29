71659a89a16058b90befe236f0eaec6a7f9ad703
Francisco Somare <somarefrancisco@gmail.com>
2020-04-29 15:25:34 -0300

    FS - TEST-01 - add Jaeger conf

--- generators/microservice/templates/app/src/main/java/Application.java
+++ generators/microservice/templates/app/src/main/java/Application.java
@@ -2,9 +2,20 @@ package <%= packageName %>;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.uber.jaeger.Configuration;
+import com.uber.jaeger.samplers.ProbabilisticSampler;
+import io.opentracing.Tracer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+    @Bean
+    public Tracer jaegerTracer() {
+        return new Configuration("spring-boot", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+                new Configuration.ReporterConfiguration())
+                .getTracer();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
